2efb795f3cb4839846125471cab9f4c342fa58c4
gabrez1985 <66213924+gabrez1985@users.noreply.github.com>
2020-06-09 18:39:31 -0500

    Add files via upload

---
+++ Proyecto1/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+
+# a cada función creada en este archivo views en django se le llama vista
+def saludo(request):
+
+    return HttpResponse("Hola Gabriel Rezqallah Django")
+
